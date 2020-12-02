12ac4a4a22972e25a480f840f37525aad79c3cb3
Harsh Singh <harshsin@usc.edu>
2020-12-02 00:28:29 -0800

    Update map to set

--- src/dev/harshsingh/day1/Main.java
+++ src/dev/harshsingh/day1/Main.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.HashSet;
 
 public class Main {
   private static final int TARGET = 2020;
@@ -16,16 +16,16 @@ public class Main {
   }
 
   public static void get2Sum2020(ArrayList<Integer> list) {
-    HashMap<Integer, Integer> map = new HashMap<>();
+    HashSet<Integer> set = new HashSet<>();
 
     for (int num : list) {
       int diff = TARGET - num;
 
-      if (map.containsKey(diff)) {
-        System.out.println("Found pair that sums to " + TARGET + "num 1: " + map.get(diff) + " num 2: " + num);
-        System.out.println("Their product: " + map.get(diff)*num);
+      if (set.contains(diff)) {
+        System.out.println("Found pair that sums to " + TARGET + "num 1: " + diff + " num 2: " + num);
+        System.out.println("Their product: " + diff*num);
       } else {
-        map.put(num, num);
+        set.add(num);
       }
     }
   }
